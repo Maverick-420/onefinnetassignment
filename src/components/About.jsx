589dc7fd7ae1593399412b34b62d0293d8195027
Aryan Kumar <aryan.ak.work@gmail.com>
2024-11-25 17:29:25 +0530

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="container mx-auto px-4 py-16 font-racingOneSans">
+      <h1 className="text-4xl font-bold mb-8">About</h1>
+      <div className="`">
+        <p>
+          Welcome to GearUp Cycles, where passion meets performance. We
+          specialize in crafting high-quality bicycles designed for adventure
+          enthusiasts, urban commuters, and cycling hobbyists. Our commitment to
+          innovation, durability, and style sets us apart in the cycling
+          industry. At the heart of our collection is the Kryo X26 MTB, a bike
+          engineered for both rugged trails and smooth city rides. With its
+          lightweight frame, steel suspension fork, and robust hardtail design,
+          the Kryo X26 MTB redefines versatility and comfort. Our mission is to
+          inspire every cyclist, whether you're a seasoned rider or a beginner,
+          to explore the world on two wheels. From mountain bikes to road bikes,
+          we bring you top-notch craftsmanship paired with exceptional value.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Contact() {
+
+  return (
+    <div className="container mx-auto px-4 py-16 font-racingOneSans">
+      <h1 className="text-4xl font-bold mb-8">Contact</h1>
+        <p>Address: New Delhi, India</p>
+        <p>Phone: +91-0000000000</p>
+        <p>Email: gearupcycles@gmail.com</p>
+    </div>
+  );
+}
